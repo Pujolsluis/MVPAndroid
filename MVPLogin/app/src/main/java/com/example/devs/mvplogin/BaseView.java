4c9378cc07de40481ca2ca1dbb5e410f8ad8b47d
Luis Pujols <luispujols@hotmail.com>
2017-08-25 15:48:56 -0400

    ADD 'Created the base view and the base presenter interfaces'

---
+++ MVPLogin/app/src/main/java/com/example/devs/mvplogin/BaseView.java
@@ -0,0 +1,11 @@
+package com.example.devs.mvplogin;
+
+/**
+ * Created by devs on 8/25/17.
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
---
+++ MVPLogin/app/src/main/java/com/example/devs/mvplogin/login/LoginContract.java
@@ -0,0 +1,10 @@
+package com.example.devs.mvplogin.login;
+
+/**
+ * Created by devs on 8/25/17.
+ */
+
+public interface LoginContract {
+
+
+}
