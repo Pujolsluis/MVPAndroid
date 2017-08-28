1d68fe37cb986ca825d68a46c7cfa727761229d2
Luis Pujols <luispujols@hotmail.com>
2017-08-27 22:00:05 -0400

    ADD 'Created the LoginFragment'

--- MVPLogin/app/src/main/java/com/example/devs/mvplogin/login/LoginContract.java
+++ MVPLogin/app/src/main/java/com/example/devs/mvplogin/login/LoginContract.java
@@ -2,6 +2,7 @@ package com.example.devs.mvplogin.login;
 
 import com.example.devs.mvplogin.BasePresenter;
 import com.example.devs.mvplogin.BaseView;
+import com.example.devs.mvplogin.data.User;
 
 /**
  * Created by devs on 8/25/17.
@@ -10,9 +11,15 @@ import com.example.devs.mvplogin.BaseView;
 public interface LoginContract {
     interface View extends BaseView<Presenter>{
 
+        void setLoadingIndicator(boolean active);
+
+        void showLoginError();
+
     }
 
     interface Presenter extends BasePresenter {
+
+        void login(User user);
 
     }
 
