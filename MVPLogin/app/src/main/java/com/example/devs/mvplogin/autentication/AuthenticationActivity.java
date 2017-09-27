b24fdeb3cf784caa7f6d889aa9e8d4e348e2ee7d
Luis Pujols <luispujols@hotmail.com>
2017-09-27 16:56:30 -0400

    ADD "Created the layout for the signup fragment"

---
+++ MVPLogin/app/src/main/java/com/example/devs/mvplogin/autentication/AuthenticationActivity.java
@@ -0,0 +1,60 @@
+package com.example.devs.mvplogin.autentication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.example.devs.mvplogin.R;
+import com.example.devs.mvplogin.autentication.login.LoginContract;
+import com.example.devs.mvplogin.autentication.login.LoginFragment;
+import com.example.devs.mvplogin.autentication.signup.SignUpFragment;
+import com.example.devs.mvplogin.util.ActivityUtils;
+
+public class AuthenticationActivity extends AppCompatActivity implements LoginContract.View.onLoginButtonClickListener, LoginContract.View.onSignupClickListener {
+
+    private static final String loginFragmentName = "LoginFragment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoginFragment mainActivityFragment = (LoginFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mainActivity_fragment);
+
+        //TODO IMPLEMENT FRAGMENT IN THE ACTIVITY
+        if(mainActivityFragment == null){
+
+            mainActivityFragment = LoginFragment.newInstance();
+
+//            FragmentManager fragmentManager = getSupportFragmentManager();
+//
+//            fragmentManager.beginTransaction()
+//                    .add(R.id.mainActivity_fragment, mainActivityFragment)
+//                    .commit();
+
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
+                    mainActivityFragment, R.id.mainActivity_fragment);
+
+        }
+
+
+    }
+
+
+    @Override
+    public void onLoginClicked() {
+        Toast.makeText(this, "You have logged in succesfully", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSignupClicked() {
+
+        SignUpFragment signUpFragment = (SignUpFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mainActivity_fragment);
+
+        ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
+                signUpFragment, R.id.mainActivity_fragment);
+
+    }
+}
