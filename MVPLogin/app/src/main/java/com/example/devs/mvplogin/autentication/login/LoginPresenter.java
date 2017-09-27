ad8a874092e58c9a160a612cd9e41e67df1806f4
Luis Pujols <luispujols@hotmail.com>
2017-09-26 21:08:16 -0400

    UPDATE "Extracted the gradle dependecies versions into the main gradle file, and started the implementation of the presenter for the login fragment"

--- MVPLogin/app/src/main/java/com/example/devs/mvplogin/autentication/login/LoginPresenter.java
+++ MVPLogin/app/src/main/java/com/example/devs/mvplogin/autentication/login/LoginPresenter.java
@@ -1,6 +1,12 @@
 package com.example.devs.mvplogin.autentication.login;
 
+import android.support.annotation.NonNull;
+
 import com.example.devs.mvplogin.data.User;
+import com.example.devs.mvplogin.data.source.UserRepository;
+
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by devs on 9/15/17.
@@ -8,8 +14,18 @@ import com.example.devs.mvplogin.data.User;
 
 public class LoginPresenter implements LoginContract.Presenter {
 
+    private final UserRepository mUserRepository;
+
+    private final LoginContract.View mLoginView;
 
 
+    public LoginPresenter(@NonNull UserRepository userRepository,
+                          @NonNull LoginContract.View loginView){
+        mUserRepository = checkNotNull(userRepository, "userRepository cannot be null!");
+        mLoginView = checkNotNull(loginView, "loginView cannot be null!");
+
+
+    }
 
 
 
